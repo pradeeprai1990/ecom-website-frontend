d1abb841da793ed3d385a24dc17952b46eb25cce
pradeeprai1990 <pradeep.9997@gmail.com>
2025-06-28 22:56:15 +0530

    Latest Code

--- src/app/HomeComponents/FeaturedProduct.jsx
+++ src/app/HomeComponents/FeaturedProduct.jsx
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import Link from 'next/link';
 import React, { useState } from 'react'
 import { FaRegHeart } from "react-icons/fa";
+import { useSelector } from 'react-redux';
 
 export default function FeaturedProduct({ productImagePath, productData, productType, setproductType }) {
 
@@ -19,35 +21,9 @@ export default function FeaturedProduct({ productImagePath, productData, product
                         {
                             productData.map((items, index) => {
                                 return (
-                                    <div className='shadow-lg shadow-gray-300 bg-white '>
-                                       <Link href={`/product-details/${items.slug}`}>
-                                            <figure>
-                                                <img src={productImagePath+items.productImage} alt="" />
-                                            </figure>
-                                            <div className='text-center '>
-                                                <p className='text-sm py-3'>
-                                                {items.subSubCategory.subSubcategoryName}
-                                                    
-                                                    </p>
-                                                <h2 className='text-[15px] font-semibold hover:text-[#C09578] hover:cursor-pointer'>
-                                                    {items.productName}
-                                                </h2>
-                                                <hr className='w-[80%] mx-auto border-1 h-px border-gray-100 my-2' />
-                                                <p className='py-3'>
-                                                    <span className='line-through text-gray-600 font-semibold text-[14px]'>Rs. {items.productActualPrice}</span>&nbsp;&nbsp;
-                                                    <span className='text-[16px] text-[#C09578] font-bold'>Rs. {items.productsalePrice}</span>
-                                                </p>
-                                                <div className='flex gap-1 justify-center my-5'>
-                                                    <div className='w-[35px] h-[35px] bg-gray-200 flex justify-center items-center hover:bg-[#C09578] hover:cursor-pointer'>
-                                                        <FaRegHeart className='text-xl' />
-                                                    </div>
-                                                    <div>
-                                                        <button className='capitalize border-0 bg-gray-200 text-sm text-gray-700 border-gray-300 h-[35px] px-4 hover:cursor-pointer hover:bg-[#C09578] '>add to cart</button>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </Link>
-                                    </div>
+
+                                    <ProductItems items={items} productImagePath={productImagePath} key={index} />
+
                                 )
                             })
                         }
@@ -65,5 +41,86 @@ export default function FeaturedProduct({ productImagePath, productData, product
             </section>
 
         </>
+    )
+}
+
+function ProductItems({ items, productImagePath }) {
+    let token = useSelector((store) => store.login.token)
+    let [color, setColor] = useState(items.productColor[0]._id)
+    console.log(items)
+    let user = useSelector((store) => store.login.user)
+    let apiBaseUrl = process.env.NEXT_PUBLIC_APIBASEURL
+    let addToCart = () => {
+        if (user) {
+            let obj = {
+                id: items._id,
+                image: items.productImage,
+                price: items.productsalePrice,
+                qty: 1,
+                title: items.productName,
+                color
+            }
+
+           
+            axios.post(
+                `${apiBaseUrl}cart/add-to-cart`,
+                obj,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                })
+                .then((res) => {
+                    if (res.data.status) {
+    
+                    }
+                    else {
+                        alert(res.data.msg)
+                    }
+                })
+        }
+        else {
+            alert("Please Login")
+        }
+    }
+    return (
+        <div className='shadow-lg shadow-gray-300 bg-white '>
+            <Link href={`/product-details/${items.slug}`}>
+                <figure>
+                    <img src={productImagePath + items.productImage} alt="" />
+                </figure>
+            </Link>
+            <div className='text-center '>
+                <p className='text-sm py-3'>
+                    {items.subSubCategory.subSubcategoryName}
+
+                </p>
+                <h2 className='text-[15px] font-semibold hover:text-[#C09578] hover:cursor-pointer'>
+                    {items.productName}
+                </h2>
+                <hr className='w-[80%] mx-auto border-1 h-px border-gray-100 my-2' />
+                <p className='py-3'>
+                    <span className='line-through text-gray-600 font-semibold text-[14px]'>Rs. {items.productActualPrice}</span>&nbsp;&nbsp;
+                    <span className='text-[16px] text-[#C09578] font-bold'>Rs. {items.productsalePrice}</span>
+                </p>
+                <div className='flex gap-1 justify-center my-5'>
+                    <div className=' h-[35px] bg-gray-200 flex justify-center items-center hover:bg-[#C09578] hover:cursor-pointer'>
+                        <select onChange={(e) => setColor(e.target.value)}>
+                            {
+
+                                items.productColor.map((colors, index) => <option value={colors._id}> {colors.colorName} </option>)
+
+
+                            }
+
+                        </select>
+                    </div>
+                    <div>
+                        <button onClick={addToCart} className='capitalize border-0 bg-gray-200 text-sm text-gray-700 border-gray-300 h-[35px] px-4 hover:cursor-pointer hover:bg-[#C09578] '>add to cart</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
